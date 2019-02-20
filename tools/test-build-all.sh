1863dc9ae016185eec19eb2b3c92d92986cadc3f
Jonas Claeson <jonas.cl@protonmail.com>
2019-02-20 16:16:50 +0100

    Add script that builds all combinations
    All combinations of platforms, toolchains and native apps.

---
+++ tools/test-build-all.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Run from root directory
+
+PLATFORMS="beaglebone versatilepb"
+TOOLCHAINS="gcc clang-gcc"
+APPS="native-hello-world native-newlib"
+
+for plat in ${PLATFORMS} ; do
+  for toolc in ${TOOLCHAINS} ; do
+    # Create build directory
+    dir=$(mktemp -d -p . test.$plat.$toolc.XXXXXX)
+    # Configure and build
+    (cd $dir &&
+     echo $dir &&
+     cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=../core/tools/$toolc-toolchain.cmake -DPLATFORM=$plat .. &&
+     ninja $APPS
+    )
+    # Check if build passed
+    if [ $? -eq 0 ]
+    then
+      echo "Build passed for $plat $toolc"
+    else
+      echo "Build failed for $plat $toolc"
+      exit 1
+    fi
+  done
+done
+
+echo "All builds passed!"
