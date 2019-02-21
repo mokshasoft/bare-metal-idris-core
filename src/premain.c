92df6f881a73fa59a9e8486e11ef40d051c15a9c
Jonas Claeson <jonas.cl@protonmail.com>
2019-02-21 13:02:25 +0100

    Fix bss linker section error
    The linker used text instead of bss. Add clearing of bss section
    in premain.c.
    
    The Idris hello-world app now runs on BeagleBone hardware!

--- src/premain.c
+++ src/premain.c
@@ -9,7 +9,21 @@
 // Defined by the Idris RTS library in idris_main.c
 extern int main();
 
+extern int __BssStart;
+static unsigned char *pBssStart = (unsigned char *)&__BssStart;
+extern int __BssEnd;
+static unsigned char *pBssEnd = (unsigned char *)&__BssEnd;
+
+void clear_bss()
+{
+    unsigned char *ptr;
+    for (ptr = pBssStart; ptr < pBssEnd; ptr++) {
+        *ptr = 0;
+    }
+}
+
 int premain(void)
 {
+    clear_bss();
     return main();
 }
