0be7d1d922d3f354f9965221b61741db20670f71
Jonas Claeson <jonas.cl@protonmail.com>
2020-01-06 21:48:31 +0100

    Create a native app that blinks the BBB onboard LEDs

---
+++ src/native/native-led-blink.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019, Mokshasoft AB (mokshasoft.com)
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+extern void _put32(unsigned int, unsigned int);
+extern unsigned int _get32(unsigned int);
+
+#define CM_PER_GPIO1_CLKCTRL 0xAC
+#define CM_PER_GPIO1_CLKCTRL_OPTFCLKEN_GPIO_1_GDBCLK 0x00040000
+#define CM_PER_GPIO1_CLKCTRL_MODULEMODE_ENABLE       0x2
+#define GPIO_OE              0x134
+#define GPIO_CLEARDATAOUT    0x190
+#define GPIO_SETDATAOUT      0x194
+#define SOC_PRCM_REGS        0x44E00000
+#define SOC_GPIO_1_REGS      0x4804C000
+
+// Busy wait time
+#define TIME 50000000
+
+void busywait(volatile unsigned int time)
+{
+    int i = 0;
+    for (i = 0; i < time; i++);
+}
+
+int main(void)
+{
+    volatile unsigned int i;
+    // Init GPIO
+    _put32(SOC_PRCM_REGS + CM_PER_GPIO1_CLKCTRL,
+        CM_PER_GPIO1_CLKCTRL_OPTFCLKEN_GPIO_1_GDBCLK | CM_PER_GPIO1_CLKCTRL_MODULEMODE_ENABLE);
+    // Set GPIO direction to output
+    i = _get32(SOC_GPIO_1_REGS + GPIO_OE);
+    i &= ~(0xF << 21);
+    _put32(SOC_GPIO_1_REGS + GPIO_OE, i);
+
+    // Blick LEDs forever
+    i = 0;
+    for (;;) {
+        // Blink once
+        _put32(SOC_GPIO_1_REGS + GPIO_SETDATAOUT, (1 << i) << 21);
+        busywait(TIME);
+        _put32(SOC_GPIO_1_REGS + GPIO_CLEARDATAOUT, (1 << i) << 21);
+        busywait(TIME);
+        // Move on to next LED
+        i = (i + 1) % 4;
+    }
+    return 0;
+}
